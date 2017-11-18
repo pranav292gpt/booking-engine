eef7207eb35dcfd121d6e2f7ce2f688c55ba8598
Po <Pranav>
2017-11-18 18:29:08 +0530

    Added Inventory model

--- engine/db/models/inventory.py
+++ engine/db/models/inventory.py
@@ -2,3 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
+from Timestampedmodel import Timestampedmodel
+
+# Timestamped Model for maintaining vehicle inventory
+
+# TODO: Add Sites/Station model
+
+class Inventory(Timestampedmodel):
+    is_active = models.BooleanField(default=True)
+    quantity = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
