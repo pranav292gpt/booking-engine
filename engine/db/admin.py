93d2bfe2cbbc62fac9451e864d0082054bde0e01
Po <Pranav>
2017-11-20 13:01:26 +0530

    Added Booking and Inventory model to Admin

--- engine/db/admin.py
+++ engine/db/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import Inventory
+from models import Booking
 
-# Register your models here.
+admin.site.register(Inventory)
+admin.site.register(Booking)
