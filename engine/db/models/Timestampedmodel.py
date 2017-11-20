ddf7831f5722dc360985f34feb947d95d5587a48
Po <Pranav>
2017-11-20 10:56:22 +0530

    Comment added for Timestamped model

--- engine/db/models/Timestampedmodel.py
+++ engine/db/models/Timestampedmodel.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+#Timestamped model from which all other models to be inherited to keep track of creation and updation time
 class Timestampedmodel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
