ac046a87b5c34367024d7a5e5652e7be6435da48
Po <Pranav>
2017-11-21 00:37:48 +0530

    Updated admin for User

--- engine/db/admin.py
+++ engine/db/admin.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Inventory
-from models import Booking
+from models import Inventory, User, Booking
 
 admin.site.register(Inventory)
 admin.site.register(Booking)
+admin.site.register(User)
