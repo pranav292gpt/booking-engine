096557e88c2932840fb286ada29b1e9b229d3ff9
Po <Pranav>
2017-11-28 21:00:18 +0530

    Added refferals system to user model and migrations

--- engine/db/models/user.py
+++ engine/db/models/user.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import UserManager
 from Timestampedmodel import Timestampedmodel
 from django.contrib.postgres.fields import ArrayField
 
-#User model to replace Django's default User
+#User model to replace Django's default User.
 class User(AbstractBaseUser, Timestampedmodel):
     first_name = models.CharField(max_length=64, blank=True, null=True)
     last_name = models.CharField(max_length=64, blank=True, null=True)
@@ -13,7 +13,26 @@ class User(AbstractBaseUser, Timestampedmodel):
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+
+
+    install_type_choices = (
+        ("o", "Organic"),
+        ("r", "Referral"),
+        )
+
+    #Refferal fields
+    #Array field with list of all the users primary key referred by the user.
+    refferals = ArrayField(models.IntegerField(), blank=True, null=True)
+
+    #Integer field containing primart key of the user whose refferal code was used while creating account.
+    reffered_by = models.IntegerField(blank=True, null=True)
    
+    #Refferal code of the user which can be used for reffering other users.
+    refferal_code = models.CharField(max_length=16,null=True)
+
+    install_type = models.CharField(max_length=32, choices=install_type_choices, default='o')
+
+
     objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
