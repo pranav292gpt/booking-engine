00708a8644fa90f742e8846f4f5c0601a7790609
Po <Pranav>
2017-12-24 14:04:34 +0530

    Booking status update using celery periodic beacon

--- engine/api/tasks.py
+++ engine/api/tasks.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from django.core.mail import send_mail
+from db.models import Booking
+import datetime
+from datetime import timedelta
+from django.db.models import Q
 
 @shared_task
 def send_verification_mail(reciever, code):
@@ -8,3 +12,27 @@ def send_verification_mail(reciever, code):
     message = "Your verification code is {0}.".format(code)
     sender = 'pranav292gpt@gmail.com'
     send_mail(subject, message, sender, [reciever], fail_silently=False)
+    return code
+
+@shared_task
+def send_booking_status_mail(booking_object):
+    email = booking_object.user.email
+    if email:
+        subject = 'Booking notification'
+        message = "Dear user. You have a booking tomorrow"
+        sender = 'pranav292gpt@gmail.com'
+        send_mail(subject, message, sender, [email], fail_silently=False)
+        print email
+        return "Verification mail sent"
+
+@shared_task
+def booking_status_update():
+    today = datetime.date.today()
+    tomorrow = today + timedelta(days=1)
+
+    bookings = Booking.objects.filter(status__in=[1,2])
+    bookings = bookings.filter(start_time__lte=tomorrow, end_time__gt=today)
+    print bookings
+    for object in bookings:
+        send_booking_status_mail(object)
+        return 'sent mail'
--- engine/engine/celery.py
+++ engine/engine/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engine.settings')
 
@@ -13,3 +14,17 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@app.task
+def add(x, y):
+    return x + y
+
+app.conf.beat_schedule = {
+    'add-everyday at 8am and 8pm': {
+        'task': 'api.tasks.booking_status_update',
+        'schedule': crontab(hour='8,16', minute=0),
+        'args': []
+    },
+}
+app.conf.timezone = 'UTC'
